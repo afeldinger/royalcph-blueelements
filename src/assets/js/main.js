f78922e7f3761b8b5382be0c77d57c336846bb15
Anders Feldinger <af@andersfeldinger.dk>
2015-04-08 14:58:41 +0200

    draggable viewfinder

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,13 +2,20 @@
 
 $(document).ready(function() {
 
+    $('#snapshot-viewport').draggable(
+        {
+            containment: 'parent',
+            opacity: 0.5
+        }
+    );
+
 	// init flexsliders on page
 	$('.flexslider').flexslider({
 		animation: 'slide',
 		slideshow: false,
 	});
 
-
+	// handle video links in a lightbox
 	$('a[href*="vimeo.com"], a[href*="youtube.com"]').magnificPopup({
 		type: 'iframe',
 	});
