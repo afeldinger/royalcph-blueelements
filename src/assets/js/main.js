1911b474301ba95e6159d84f242304f0a12a6a20
Anders Feldinger <af@andersfeldinger.dk>
2015-04-09 13:17:26 +0200

    added correct webfonts, updated dist/.htaccess

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,17 +2,46 @@
 (function() {
 	'use strict';
 
+	var server_uri = 'http://blueelements.rc.magnetix.dk';
 
 	var touchevents = function() {
 		return (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch)? true:false;
 	};
 
+    function getImageSnapList(countRequest) {
+        var request = {
+            imageSnapCountRequest: countRequest
+        };
+
+        $.ajax({
+            async: true,
+            url: server_uri + '/umbraco/surface/ImageSnapSurface/GetImageSnaps',
+            type: "post",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(request),
+            success: function(result) {
+                if (result.success) {
+                    return result.snapList;
+                } else {
+                    console.log(result.error);
+                }
+            },
+            error: function() {
+                console.log('Error fetching image snaps');
+            }
+        });
+    }
 
 	$(document).ready(function() {
 
 		if (!touchevents()) {
 			$('body').addClass('no-touch');
 		}
+
+
+		var snaps = getImageSnapList(15);
+		console.log(snaps);
+
 		
 		// Move snapshot object sideways on small clients
 		$('.snapshot-view').each(function() {
@@ -27,25 +56,36 @@
 			
 		    viewfinder.draggable({
 		        containment: 'parent',
-		        opacity: 0.5
-		    }).on('dragstart', function() {
-		    	viewport_width = $(this).parents('.snapshot-view:first').width();
-		    	viewport_oob = viewport_min_width - viewport_width;
-		    }).on('drag', function() {
-		    	if (viewport_width < viewport_min_width) {
-		    		var x = $(this).offset().left;
-		    		var x_ratio = x / (viewport_width - viewfinder_width);
-		    		var x_offset = -1 * Math.ceil(x_ratio * viewport_oob);
-		    		viewport_img.css('transform', 'translate3d(' + x_offset + 'px,0,0)');
-		    	}
-		    }).on('dragstop', function() {
-		    	var pos = $(this).offset();
-		    	var x = pos.left;
-		    	var y = pos.top - $('.snapshot-form').offsetParent().offset().top;
-		    	$('.snapshot-form').css({'left':x, 'top':y, 'height':$(this).height()});
+		        opacity: 0.5,
+		        start: function() {
+			    	viewport_width = $(this).parents('.snapshot-view:first').width();
+			    	viewport_oob = viewport_min_width - viewport_width;
+			    },
+			    drag: function() {
+			    	if (viewport_width < viewport_min_width) {
+			    		var x = $(this).offset().left;
+			    		var x_ratio = x / (viewport_width - viewfinder_width);
+			    		var x_offset = -1 * Math.ceil(x_ratio * viewport_oob);
+			    		viewport_img.css('transform', 'translate(' + x_offset + 'px,0)');
+			    	}
+			    },
+			    stop: function(event, ui) {
+
+			        // event.toElement is the element that was responsible
+			        // for triggering this event. The handle, in case of a draggable.
+			        $( event.originalEvent.target ).one('click', function(e){
+			        	e.stopImmediatePropagation(); 
+			       	});
+
+			    	var pos = $(this).offset();
+			    	var x = pos.left;
+			    	var y = pos.top - $('.snapshot-form').offsetParent().offset().top;
+			    	$('.snapshot-form').css({'left':x, 'top':y, 'height':$(this).height()});
+			    }
+		    }).click(function(event) {
+		    	$('.snapshot-form').addClass('active');
 		    }).find('a').click(function(event) {
 		    	event.preventDefault();
-		    	$('.snapshot-form').addClass('active');
 		    });
 		});
 
